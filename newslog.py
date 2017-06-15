da691e33c1f569449acd543dd056433a7aa0fcee
kl1010 <tilltheendoflife@gmail.com>
2017-06-14 23:02:34 -0400

    Pep8 Fixes

--- newslog.py
+++ newslog.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2.7
+
 from flask import Flask, render_template
 import newslogdb
 
@@ -8,13 +10,13 @@ app = Flask(__name__)
 def hello_world():
     all_atricles = newslogdb.articles()
     all_authors = newslogdb.authors()
-    all_errors = newslogdb.all()
+    all_errors = newslogdb.errors()
 
-    return render_template('index.html',title='Log Dashboard',
-                           all_articles = all_atricles,
-                           all_authors = all_authors,
+    return render_template('index.html', title='Log Dashboard',
+                           all_articles=all_atricles,
+                           all_authors=all_authors,
                            all_errors=all_errors)
 
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
--- newslogdb.py
+++ newslogdb.py
@@ -1,4 +1,4 @@
-# "Database code" for the DB Forum.
+#!/usr/bin/env python2.7
 
 import datetime
 import time
@@ -7,14 +7,16 @@ import bleach
 
 DBNAME = "news"
 
-# There are 3 tables authors , articles, and log
 
 def authors():
-    for_data =[]
+    for_data = []
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     print "Connected"
-    query = '''select authors.name as aun, count(authors.name) as cnt from log join articles on substring(log.path from 10) = articles.slug join authors on authors.id = articles.author where status = '200 OK' and path != '/' group by aun order by cnt desc'''
+    query = '''select authors.name as aun, count(authors.name) as cnt
+     from log join articles on substring(log.path from 10) = articles.slug
+      join authors on authors.id = articles.author where status = '200 OK'
+       and path != '/' group by aun order by cnt desc'''
     c.execute(query)
     try:
         data = c.fetchall()
@@ -22,15 +24,19 @@ def authors():
         data = None
     db.close()
     for x in data:
-        for_data.append([x[0],int(x[1])])
+        for_data.append([x[0], int(x[1])])
     return for_data
+
 
 def articles():
     for_data = []
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     print "Connected"
-    query = "select count(path) as cnt, title from log join articles on substring(log.path from 10) = articles.slug where status = '200 OK' group by path,slug,title order by cnt desc limit 3"
+    query = '''select count(path) as cnt, title from log
+     join articles on substring(log.path from 10) = articles.slug
+      where status = '200 OK' group by path,slug,title
+       order by cnt desc limit 3'''
     c.execute(query)
     try:
         data = c.fetchall()
@@ -38,15 +44,17 @@ def articles():
         data = None
     db.close()
     for x in data:
-        for_data.append([int(x[0]),x[1]])
+        for_data.append([int(x[0]), x[1]])
     return for_data
 
 
-def all():
+def errors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     print "Connected"
-    query = "select date(time),round(100.0*sum(case log.status when '200 OK' then 0 else 1 end)/count(log.status),2) as PE from log group by date(time) order by PE desc limit 1"
+    query = '''select date(time), round(100.0*sum(case
+     log.status when '200 OK' then 0 else 1 end)/count(log.status),2)
+     as PE from log group by date(time) order by PE desc limit 1'''
     c.execute(query)
     try:
         data = c.fetchall()
@@ -55,5 +63,5 @@ def all():
     date = data[0][0].strftime('%B %d,%Y')
     percent = float(data[0][1])
 
-    for_data = [date,percent]
+    for_data = [date, percent]
     return for_data
